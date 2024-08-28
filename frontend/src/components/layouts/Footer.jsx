4468759cbe723c0684b20e85644501bde02c408a
Anuragkhanderao <anuragkhanderao2003@gmail.com>
2024-08-28 15:20:53 +0530

    first commit

---
+++ frontend/src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <p className="text-center">
+            Food Delivery website  - 2024-25 All rights Reserved by WS.....
+        </p>
+      </footer>
+    </div>
+  )
+}
